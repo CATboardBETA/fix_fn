e14a5200d5e74b57ab8427ffd00d5565f556cc6f
CATboardBETA <66703887+CATboardBETA@users.noreply.github.com>
2024-10-27 23:01:06 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,3 @@
-//! This library enables the creation of recursive closures by providing a
-//! single macro [`fix_fn`]. The functionality is similar to the
-//! [Y combinator](https://en.wikipedia.org/wiki/Fixed-point_combinator#Fixed-point_combinators_in_lambda_calculus).
-//! Recursive closures can have arbitrary amounts of parameters and can capture
-//! variables.
-//!
-//! ```
-//! use fix_fn::fix_fn;
-//!
-//! let fib = fix_fn!(|fib, i: u32| -> u32 {
-//!     if i <= 1 {
-//!            i
-//!      } else {
-//!          // fib will call the closure recursively
-//!          fib(i - 1) + fib(i - 2)
-//!      }
-//!  });
-//!
-//! assert_eq!(fib(7), 13);
-//! ```
-//! 
-//! The generated code is not completely abstraction free as it uses one dyn trait
-//! (without any boxing) to overcome rust's recursive type limitations.
-//! In most cases, however, the optimizer should be able to eliminate any dynamic dispatch.
-//! 
-//! Unfortunately, mutable recursive closures are not supported.
-
 #[no_std]
 
 /// Takes a closure definition where the first parameter will be a [`Fn`] to the closure itself.
